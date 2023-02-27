adef87569840d6012b8a7f83f3cc193fe492f861
sahil-kaler <119391867+sahil-kaler@users.noreply.github.com>
2023-02-28 00:02:19 +0530

    Functions

--- MyfirstProject/src/Inputs.java
+++ MyfirstProject/src/Inputs.java
@@ -4,16 +4,14 @@ import java.util.Scanner;
 public class Inputs {
     public static void main(String[] args) {
         
-        // System.out.println("enter your name:");
-         Scanner input= new Scanner(System.in);
-        // String name= input.nextLine();
-        // System.out.println("Good Morning " + name);
-
-        int num= input.nextInt();
-        if (num % 2 == 0)
-        System.out.println(num + " is Even");
-        else
-        System.out.println(num + " is odd");
+        try (// System.out.println("enter your name:");
+        Scanner input = new Scanner(System.in)) {
+            int num= input.nextInt();
+            if (num % 2 == 0)
+            System.out.println(num + " is Even");
+            else
+            System.out.println(num + " is odd");
+        }
          }
 
     }
--- MyfirstProject/src/Largest.java
+++ MyfirstProject/src/Largest.java
@@ -3,19 +3,20 @@ import java.util.Scanner;
 
 public class Largest {
     public static void main(String[] args) {
-        Scanner in= new Scanner(System.in);
-        int a = in.nextInt();
-        int b = in.nextInt();
-        int c = in.nextInt();
+        try (Scanner in = new Scanner(System.in)) {
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int c = in.nextInt();
 
-        int max=a;
-        if (b>max) {
-            max=b;
+            int max=a;
+            if (b>max) {
+                max=b;
+            }
+            if (c>max) {
+                max=c;
+            }
+            System.out.println(max);
         }
-        if (c>max) {
-            max=c;
-        }
-        System.out.println(max);
     }
     
 }

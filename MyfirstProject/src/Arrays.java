5decde74c1b61a45ce00ba1c445a22a82e0c8954
sahil-kaler <119391867+sahil-kaler@users.noreply.github.com>
2023-03-02 22:29:55 +0530

    First commit

---
+++ MyfirstProject/src/Arrays.java
@@ -0,0 +1,14 @@
+
+
+public class Arrays {
+    public static void main(String[] args) {
+        int[] ros={3,4,5,6,7,8};
+        System.out.println(ros[0]);
+    
+        
+    }
+   
+    
+
+    
+}

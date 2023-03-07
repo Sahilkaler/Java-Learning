3d08fbc57a228e9774e0951c54dbf6b5adf5dd1d
sahil-kaler <119391867+sahil-kaler@users.noreply.github.com>
2023-03-07 23:36:48 +0530

    First Commit

---
+++ MyfirstProject/src/Max.java
@@ -0,0 +1,19 @@
+public class Max {
+    public static void main(String[] args) {
+        int[] arr = {5,99,108,7,5};
+        System.out.println(max(arr));
+    
+    }
+    static int max(int[] arr){
+        int maxval = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > maxval) {
+                maxval = arr[i];
+                
+            }
+            
+        }
+        return maxval;
+    }
+    
+}

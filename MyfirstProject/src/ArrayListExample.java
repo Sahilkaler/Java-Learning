14b5d42da3d0ef6eeac1e85338c3e591c526db8f
sahil-kaler <119391867+sahil-kaler@users.noreply.github.com>
2023-03-07 22:59:06 +0530

    Learning Array

---
+++ MyfirstProject/src/ArrayListExample.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class ArrayListExample {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        //syntax
+        ArrayList<Integer> list = new ArrayList<Integer>(10);
+
+       // list.add(77);
+       //list.add(8877);
+       //list.add(75527);
+       //list.add(73657);
+       //list.add(9879777);
+       //list.add(74647);
+
+      //System.out.println(list.contains(8877));
+      //list.set(0, 8569);
+
+      //System.out.println(list);
+
+        //input
+        for (int i = 0; i < 5; i++) {
+            list.add(in.nextInt());    
+        }
+        for (int i = 0; i < 5; i++) {
+            System.out.println(list.get(i));   
+        }
+
+        
+    }
+    
+}
